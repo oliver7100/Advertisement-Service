8f4b27fb317c50b949d36b94f5823fe11cf4ad2d
Your Name <you@example.com>
2023-02-14 12:10:07 +0100

    .

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net"
 
-	"github.com/oliver7100/advertisement-server/proto"
+	"github.com/oliver7100/advertisement-service/database"
 	"github.com/oliver7100/advertisement-service/proto"
 	"google.golang.org/grpc"
 )
@@ -15,11 +15,19 @@ func main() {
 		panic(err)
 	}
 
+	d, err := database.NewDatabaseConnection(
+		"root:root@tcp(127.0.0.1:3306)/db_user_service?charset=utf8mb4&parseTime=True&loc=Local",
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
 	s := grpc.NewServer()
 
 	proto.RegisterAdvertisementServiceServer(
 		s,
-		proto.NewService(),
+		proto.NewService(d),
 	)
 
 	panic(s.Serve(listener))
